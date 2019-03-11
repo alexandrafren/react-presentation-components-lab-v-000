d07d85dbeff17308dbc76ab86045037c2418def8
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-11 13:58:43 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -3,4 +3,11 @@ import React, { Component } from 'react'
 
 export default class SimpleComponent extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      mood: 'happy'
+    }
+  }
+
 }
