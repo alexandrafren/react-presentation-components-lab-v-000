a37ae9cfc4f14aac1efdfce86619e2d3f874a746
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-11 14:00:46 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -11,7 +11,16 @@ export default class SimpleComponent extends Component {
   }
 
   handleClick = () => {
-
+    if this.state.mood === 'happy' {
+      this.setState({
+        mood: 'sad'
+      })
+    }
+    else {
+      this.setState({
+        mood: 'happy'
+      })
+    }
   }
 
   render(){
